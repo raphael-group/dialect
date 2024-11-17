c3a8dba289ed2109c3960fe8f242786149dfeeef
ashuaibi7 <ashuaibi@princeton.edu>
2024-11-17 13:42:42 -0500

    initialize DIALECT main function and incorporate call to generate background mutation rate distribution files using CBaSE

---
+++ src/dialect/__main__.py
@@ -0,0 +1,30 @@
+import logging
+from dialect.utils.logger import configure_logging
+
+from dialect.utils.argument_parser import (
+    build_argument_parser,
+)  # TODO: export function from argument_parser.py
+
+from dialect.utils.generate import (
+    generate_bmr_and_counts,
+)  # TODO: export function from generate.py
+
+
+configure_logging()
+
+
+def main():
+    logging.info("Running DIALECT CLI")
+
+    parser = build_argument_parser()
+    args = parser.parse_args()
+
+    logging.info(f"Arguments: {args}")
+    logging.info(f"Command: {args.command}")
+
+    if args.command == "generate":
+        generate_bmr_and_counts(maf=args.maf, out=args.out, reference=args.reference)
+
+
+if __name__ == "__main__":
+    main()
