1581cfe02c88d882816fe9b88eaf822b468ee166
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-12 10:11:35 -0500

    modified argumenet name for identify parser to be more accurate and added todo comments for remaining parsers to create

--- src/dialect/utils/argument_parser.py
+++ src/dialect/utils/argument_parser.py
@@ -31,7 +31,7 @@ def build_argument_parser():
         "identify", help="Run DIALECT to identify interactions"
     )
     identify_parser.add_argument(
-        "-m", "--maf", required=True, help="Path to the input MAF file"
+        "-c", "--cnt", required=True, help="Path to the input count matrix file"
     )
     identify_parser.add_argument(
         "-b", "--bmr", required=True, help="Path to the BMR file"
@@ -47,39 +47,8 @@ def build_argument_parser():
         help="Number of genes to consider (default: 100 genes with highest mutation count)",
     )
 
-    # # Subparser for comparison analysis
-    # compare_parser = subparsers.add_parser("compare", help="Run comparison analysis")
-    # compare_parser.add_argument("method", choices=["fishers", "discover", "wesme"])
-    # compare_parser.add_argument("cnt_mtx_fn", help="Path to the count matrix file")
-    # compare_parser.add_argument("dout", help="Path to the output directory")
-    # compare_parser.add_argument("--top_k", default=100, type=int)
-    # compare_parser.add_argument(
-    #     "--feature_level",
-    #     default="mutation",
-    #     choices=["gene", "mutation"],
-    #     help="Feature level to use for comparison test",
-    # )
-
-    # # Subparser for workflows
-    # workflow_parser = subparsers.add_parser("workflow", help="Run workflows")
-    # workflow_parser.add_argument(
-    #     "workflow_name",
-    #     help="Name of the workflow to execute (e.g., generate, analyze, compare)",
-    # )
-    # workflow_parser.add_argument(
-    #     "--snakefile", default="Snakefile", help="Path to the Snakefile"
-    # )
-    # workflow_parser.add_argument(
-    #     "--configfile", default=None, help="Path to the config file"
-    # )
-    # workflow_parser.add_argument(
-    #     "--cores", type=int, default=1, help="Number of cores for Snakemake"
-    # )
-    # workflow_parser.add_argument(
-    #     "--dry-run", action="store_true", help="Perform a dry run without execution"
-    # )
-    # workflow_parser.add_argument(
-    #     "--forceall", action="store_true", help="Force execution of all rules"
-    # )
+    # TODO: create subparser to run comparison methods
+    # TODO: create subparser to run simulations
+    # TODO: create subparser to run workflows end-to-end
 
     return parser
