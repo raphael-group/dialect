60199a6ac5e3fe151e7eca7f10d42970253eb750
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-16 15:41:56 -0500

    remove remaining archive files from github repo

--- archive/temp.sh
+++
@@ -1,4 +0,0 @@
-python dialect/core.py generate data/tcga_pancan_atlas_2018/PCPG.maf results/tcga_pancan_atlas_2018/ &
-python dialect/core.py generate data/tcga_pancan_atlas_2018/TGCT.maf results/tcga_pancan_atlas_2018/ &
-python dialect/core.py generate data/tcga_pancan_atlas_2018/PAAD.maf results/tcga_pancan_atlas_2018/ &
-python dialect/core.py generate data/tcga_pancan_atlas_2018/ESCA.maf results/tcga_pancan_atlas_2018/ &
