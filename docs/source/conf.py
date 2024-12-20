857355f73fe00cf92f7b378c9dede89553030e57
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-20 18:31:03 -0500

    download relevant sphinx documentation themes and plugins and set up initial docs page with logo

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "DIALECT"
+copyright = "2024, Ahmed Shuaibi"
+author = "Ahmed Shuaibi"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.png"
+html_css_files = ["custom.css"]
