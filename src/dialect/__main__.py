02d98a82f13a8fe090efb8f481ee582903c24921
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-21 11:11:37 -0500

    use helper method in place of direct code to read cbase results file before passing into method to identify pairwise interactions

--- src/dialect/__main__.py
+++ src/dialect/__main__.py
@@ -1,11 +1,11 @@
 import os
 import logging
-import pandas as pd
 from dialect.utils import (
     configure_logging,
     build_argument_parser,
     generate_bmr_and_counts,
     identify_pairwise_interactions,
+    read_cbase_results_file,
 )
 
 
@@ -25,11 +25,7 @@ def main():
 
     elif args.command == "identify":
         os.makedirs(args.out, exist_ok=True)  # create output directory if nonexistent
-        # TODO: consider moving below code to read cbase stats elsewhere
-        cbase_stats = None
-        if not args.cbase_stats is None:
-            logging.info(f"Reading CBaSE q-values file: {args.cbase_stats}")
-            cbase_stats = pd.read_csv(args.cbase_stats, sep="\t", skiprows=1)
+        cbase_stats = read_cbase_results_file(args)
         identify_pairwise_interactions(
             cnt_mtx=args.cnt,
             bmr_pmfs=args.bmr,
