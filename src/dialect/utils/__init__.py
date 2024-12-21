a18ab9029213adc4b25c5f7f75a634776bcc4943
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-21 11:10:19 -0500

    export helper method to read cbase results file for easy importing in other methods

--- src/dialect/utils/__init__.py
+++ src/dialect/utils/__init__.py
@@ -2,10 +2,12 @@ from .argument_parser import build_argument_parser
 from .generate import generate_bmr_and_counts
 from .identify import identify_pairwise_interactions
 from .logger import configure_logging
+from .helpers import read_cbase_results_file
 
 __all__ = [
     "build_argument_parser",
     "generate_bmr_and_counts",
     "identify_pairwise_interactions",
     "configure_logging",
+    "read_cbase_results_file",
 ]
