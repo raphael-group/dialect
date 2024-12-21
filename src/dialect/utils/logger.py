32d5f38cfa1375ebaefec1c2f9474094b2d1495e
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-21 10:58:24 -0500

    add verbose level for logging

--- src/dialect/utils/logger.py
+++ src/dialect/utils/logger.py
@@ -1,12 +1,31 @@
 import logging
 
+VERBOSE_LEVEL = 15  # Between INFO (20) and DEBUG (10)
+logging.addLevelName(VERBOSE_LEVEL, "VERBOSE")
 
-def configure_logging():
+
+def verbose(self, message, *args, **kwargs):
     """
-    Configures the logging format and level for the entire project.
+    Logs a message with the custom VERBOSE level.
+    """
+    if self.isEnabledFor(VERBOSE_LEVEL):
+        self._log(VERBOSE_LEVEL, message, args, **kwargs)
+
+
+logging.Logger.verbose = verbose
+
+
+def configure_logging(verbose=False):
+    """
+    Configure logging with an optional verbose mode.
+
+    :param verbose: Enable verbose logging if True. Defaults to False.
+    :type verbose: bool
     """
     logging.basicConfig(
-        level=logging.INFO,
+        level=VERBOSE_LEVEL if verbose else logging.INFO,
         format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    if verbose:
+        logging.getLogger().setLevel(VERBOSE_LEVEL)
