f180ac3d1046b14f2b75c94b7817da0397f03924
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-21 10:46:49 -0500

    add logger method to init project to export for easy use

--- src/dialect/utils/__init__.py
+++ src/dialect/utils/__init__.py
@@ -1,9 +1,11 @@
 from .argument_parser import build_argument_parser
 from .generate import generate_bmr_and_counts
 from .identify import identify_pairwise_interactions
+from .logger import configure_logging
 
 __all__ = [
     "build_argument_parser",
     "generate_bmr_and_counts",
     "identify_pairwise_interactions",
+    "configure_logging",
 ]
