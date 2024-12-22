7224afbcbba1693da1291158acb8bdb0364a67d3
ashuaibi7 <ashuaibi@princeton.edu>
2024-12-22 12:04:37 -0500

    add new initial sub parsers for running alternative methods and for simulations

--- src/dialect/utils/argument_parser.py
+++ src/dialect/utils/argument_parser.py
@@ -60,7 +60,56 @@ def build_argument_parser():
         help="Path to the cbase results file",
     )
 
-    # TODO: Build and integrate subparser compare subparser
-    # TODO: Build and integrate simulate subparser (create + evaluate)
+    # Subparser for comparing methods
+    # TODO: integrate this and separate out overlap in identify script
+    compare_parser = subparsers.add_parser("compare", help="Run comparison of methods")
+    compare_parser.add_argument(
+        "-c", "--cnt", required=True, help="Path to the input count matrix file"
+    )
+    compare_parser.add_argument(
+        "-b", "--bmr", required=True, help="Path to the BMR file"
+    )
+    compare_parser.add_argument(
+        "-o", "--out", required=True, help="Path to the output directory"
+    )
+    compare_parser.add_argument(
+        "-k",
+        "--top_k",
+        default=100,
+        type=int,
+        help="Number of genes to consider (default: 100 genes with highest mutation count)",
+    )
+
+    # Subparser for simulations
+    simulate_parser = subparsers.add_parser(
+        "simulate", help="Run simulations for evaluation and benchmarking"
+    )
+    simulate_subparsers = simulate_parser.add_subparsers(
+        dest="simulate_command", help="Available simulation commands"
+    )
+
+    # Subparser for creating simulations
+    simulate_create_parser = simulate_subparsers.add_parser(
+        "create", help="Create simulation data"
+    )
+    # TODO: add create arguments for bmr file, simulation parameters, etc.
+    simulate_create_parser.add_argument(
+        "-o",
+        "--out",
+        required=True,
+        help="Path to the output directory for simulations",
+    )
+
+    # Subparser for evaluating simulations
+    simulate_evaluate_parser = simulate_subparsers.add_parser(
+        "evaluate", help="Evaluate simulation results"
+    )
+    # TODO: add evaluate arguments to generate final tables and plots
+    simulate_evaluate_parser.add_argument(
+        "-r", "--results", required=True, help="Path to the results file"
+    )
+    simulate_evaluate_parser.add_argument(
+        "-o", "--out", required=True, help="Path to the output evaluation directory"
+    )
 
     return parser
