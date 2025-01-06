b64911e8a7265715a3d28f080b4b30ee579158d5
ashuaibi7 <ashuaibi@princeton.edu>
2025-01-06 12:16:34 -0500

    export run_comparison_methods function from initialization of utils to make calling it easier in main method

--- src/dialect/utils/__init__.py
+++ src/dialect/utils/__init__.py
@@ -3,6 +3,7 @@ from .generate import generate_bmr_and_counts
 from .identify import identify_pairwise_interactions
 from .logger import configure_logging
 from .helpers import read_cbase_results_file
+from .compare import run_comparison_methods
 
 __all__ = [
     "build_argument_parser",
@@ -10,4 +11,5 @@ __all__ = [
     "identify_pairwise_interactions",
     "configure_logging",
     "read_cbase_results_file",
+    "run_comparison_methods",
 ]
