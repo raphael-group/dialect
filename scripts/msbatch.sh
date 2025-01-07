b6b6c82d3e4a3d8b94c6dcecf63211a5b32d60ac
ashuaibi7 <ashuaibi@princeton.edu>
2025-01-07 14:55:16 -0500

    create scripts to batch run analyses across cancer subtypes

---
+++ scripts/msbatch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DATA_DIR=data/TCGA_PanCancer_Atlas_2018 ##TODO: SPECIFY DIR W/ MAF FILES
+MAF_FILES=(${DATA_DIR}/*.maf) ## Array of all MAF files in the directory
+SCRIPT_DIR=$(dirname "$0") ## Directory of the current script
+SLURM_DIR=output/slurm_logs
+
+mkdir -p ${SLURM_DIR}
+
+for MAF_FILE in "${MAF_FILES[@]}"; do
+    SUBTYPE_NAME=$(basename ${MAF_FILE} .maf)
+    SLURM_OUT_FN=${SLURM_DIR}/${SUBTYPE_NAME}.out
+
+    ## Submit job
+    sbatch -o ${SLURM_OUT_FN} -e ${SLURM_OUT_FN} ${SCRIPT_DIR}/ssbatch.sh ${MAF_FILE}
+    exit
+    sleep 0.25 ## Avoid overloading the scheduler
+done
---
+++ scripts/ssbatch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#SBATCH -N 1                  ## Node count
+#SBATCH --ntasks-per-node=8   ## CPUs per node
+#SBATCH -t 48:00:00           ## Walltime
+#SBATCH --mem=50GB            ## Memory
+#SBATCH --job-name=dialect_job ## Job name
+
+source /n/fs/ragr-research/users/ashuaibi/anaconda3/etc/profile.d/conda.sh
+conda activate dialect
+
+ORIGINAL_DIR=$(pwd)
+DISCOVER_DIR=${ORIGINAL_DIR}/external/DISCOVER
+cd ${DISCOVER_DIR}
+python setup.py install --user  # Use --user to install DISCOVER in user space
+cd ${ORIGINAL_DIR}
+
+K=100 ##TODO SPECIFY NUMBER OF TOPE GENES
+MAF_FILE=${1}                ## MAF file path
+SUBTYPE_NAME=$(basename ${MAF_FILE} .maf) ## Extract subtype name from MAF file
+OUTPUT_DIR=output/${SUBTYPE_NAME} ## Set output directory
+
+mkdir -p ${OUTPUT_DIR}
+
+## Commands to run
+dialect generate -m ${MAF_FILE} -o ${OUTPUT_DIR}
+dialect identify -c ${OUTPUT_DIR}/count_matrix.csv -b ${OUTPUT_DIR}/bmr_pmfs.csv -o ${OUTPUT_DIR} -k ${K} -cb ${OUTPUT_DIR}/CBaSE_output/q_values.txt
+dialect compare -c ${OUTPUT_DIR}/count_matrix.csv -b ${OUTPUT_DIR}/bmr_pmfs.csv -o ${OUTPUT_DIR} -k ${K}
+dialect merge -d ${OUTPUT_DIR}/pairwise_interaction_results.csv -a ${OUTPUT_DIR}/comparison_interaction_results.csv -o ${OUTPUT_DIR}
