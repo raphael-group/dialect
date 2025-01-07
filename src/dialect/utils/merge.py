d0165443d063a1d041aeae6950ebfc7339f85b04
ashuaibi7 <ashuaibi@princeton.edu>
2025-01-07 13:59:08 -0500

    update merge subcommand method to create complete pairwise ixn results file through merging

--- src/dialect/utils/merge.py
+++ src/dialect/utils/merge.py
@@ -1,13 +1,17 @@
-import numpy as np
 import pandas as pd
-
-# ---------------------------------------------------------------------------- #
-#                               HELPER FUNCTIONS                               #
-# ---------------------------------------------------------------------------- #
 
 
 # ---------------------------------------------------------------------------- #
 #                                 MAIN FUNCTION                                #
 # ---------------------------------------------------------------------------- #
-def merge_pairwise_interaction_results():
-    pass
+def merge_pairwise_interaction_results(dialect_results, alt_results, out):
+    dialect_results_df = pd.read_csv(dialect_results)
+    alt_results_df = pd.read_csv(alt_results)
+    merged_df = pd.merge(
+        dialect_results_df,
+        alt_results_df,
+        on=["Gene A", "Gene B"],
+        how="inner",
+    )
+    comparison_interaction_fout = f"{out}/complete_pairwise_ixn_results.csv"
+    merged_df.to_csv(comparison_interaction_fout, index=False)
