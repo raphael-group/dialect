8ef8fc2713601b51ffa38c8d4ed04727906f1eca
ashuaibi7 <ashuaibi@princeton.edu>
2025-01-14 18:07:15 -0500

    add in plot generation for network visualizations

--- src/dialect/utils/plotting.py
+++ src/dialect/utils/plotting.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 from itertools import product
 from matplotlib import rcParams
@@ -36,6 +37,40 @@ EDGE_COLOR = "black"
 
 
 # ---------------------------------------------------------------------------- #
+#                               HELPER FUNCTIONS                               #
+# ---------------------------------------------------------------------------- #
+def draw_network_plot(ax, G, pos, labels=None, node_colors=None):
+    nx.draw_networkx_edges(G, pos, ax=ax, edge_color=EDGE_COLOR, width=2)
+
+    if node_colors is None:
+        node_colors = [DEFAULT_GENE_COLOR] * len(G.nodes)
+
+    for node, (x, y), color in zip(G.nodes, pos.values(), node_colors):
+        label = labels[node] if labels else str(node)
+        bbox = FancyBboxPatch(
+            (x - 0.08, y),
+            0.16,
+            0,
+            boxstyle=BoxStyle.Round(pad=0.08),
+            edgecolor=EDGE_COLOR,
+            facecolor=color,
+            linewidth=1,
+        )
+        ax.add_patch(bbox)
+        ax.text(
+            x,
+            y,
+            label,
+            horizontalalignment="center",
+            verticalalignment="center",
+            fontsize=9,
+            bbox=dict(facecolor="none", edgecolor="none"),
+        )
+    ax.set_aspect("equal")
+    ax.axis("off")
+
+
+# ---------------------------------------------------------------------------- #
 #                                MAIN FUNCTIONS                                #
 # ---------------------------------------------------------------------------- #
 def plot_decoy_gene_fractions(data_filepath, out_dir):
@@ -52,7 +87,10 @@ def plot_decoy_gene_fractions(data_filepath, out_dir):
 
     # Create the Plot
     plot = (
-        ggplot(df, aes(x="Method", y="Fraction", color="Subtype", shape="Subtype"))
+        ggplot(
+            df,
+            aes(x="Method", y="Fraction", color="Subtype", shape="Subtype"),
+        )
         + geom_boxplot(
             aes(group="Method"),
             alpha=0.5,
@@ -98,32 +136,92 @@ def plot_decoy_gene_fractions(data_filepath, out_dir):
     print(f"Plot saved to {out_dir}/decoy_gene_fractions_boxplot.svg")
 
 
-def draw_network_plot(ax, G, pos, labels=None, node_colors=None):
-    nx.draw_networkx_edges(G, pos, ax=ax, edge_color=EDGE_COLOR, width=2)
+def draw_network_gridplot_across_methods(
+    top_k,
+    subtype,
+    driver_genes,
+    decoy_genes,
+    results_df,
+):
+    methods = {
+        "DIALECT": "Rho",
+        "DISCOVER": "Discover ME Q-Val",
+        "Fisher's Exact Test": "Fisher's ME Q-Val",
+        "MEGSA": "MEGSA S-Score (LRT)",
+        "WeSME": "WeSME Q-Val",
+    }
 
-    if node_colors is None:
-        node_colors = [DEFAULT_GENE_COLOR] * len(G.nodes)
+    fig, axes = plt.subplots(2, 3, figsize=(24, 16))
+    fig.suptitle(f"Top 10 Ranked ME Pairs in {subtype}", fontsize=42, y=0.999)
+    for idx, (method, col) in enumerate(methods.items()):
+        ax = axes[idx // 3, idx % 3]
+        ax.set_title(method, fontsize=36)
+        top_ranking_pairs = results_df.sort_values(
+            col, ascending=col != "MEGSA S-Score (LRT)"
+        ).head(top_k)
+        edges = top_ranking_pairs[["Gene A", "Gene B"]].values
+        G = nx.Graph()
+        G.add_edges_from(edges)
 
-    for node, (x, y), color in zip(G.nodes, pos.values(), node_colors):
-        label = labels[node] if labels else str(node)
-        bbox = FancyBboxPatch(
-            (x - 0.08, y),
-            0.16,
-            0,
-            boxstyle=BoxStyle.Round(pad=0.08),
-            edgecolor=EDGE_COLOR,
-            facecolor=color,
-            linewidth=1,
-        )
-        ax.add_patch(bbox)
-        ax.text(
-            x,
-            y,
-            label,
-            horizontalalignment="center",
-            verticalalignment="center",
-            fontsize=9,
-            bbox=dict(facecolor="none", edgecolor="none"),
-        )
-    ax.set_aspect("equal")
-    ax.axis("off")
+        node_colors = []
+        for node in G.nodes:
+            if node in decoy_genes:
+                node_colors.append(DECOY_GENE_COLOR)
+            elif node in driver_genes:
+                node_colors.append(DRIVER_GENE_COLOR)
+            else:
+                node_colors.append(DEFAULT_GENE_COLOR)
+
+        pos = nx.circular_layout(G)
+        labels = {node: node for node in G.nodes}
+        draw_network_plot(ax, G, pos, labels, node_colors)
+    plt.subplots_adjust(hspace=6, wspace=2)
+
+    legend_ax = axes[1, 2]
+    legend_ax.axis("off")
+    legend_ax.legend(
+        handles=[
+            plt.Line2D(
+                [0],
+                [0],
+                marker="o",
+                color=DRIVER_GENE_COLOR,
+                markersize=50,
+                linestyle="None",
+                label="Driver Gene",
+            ),  # Driver gene
+            plt.Line2D(
+                [0],
+                [0],
+                marker="o",
+                color=DEFAULT_GENE_COLOR,
+                markersize=50,
+                linestyle="None",
+                label="Passenger Gene",
+            ),  # Default gene
+            plt.Line2D(
+                [0],
+                [0],
+                marker="o",
+                color=DECOY_GENE_COLOR,
+                markersize=50,
+                linestyle="None",
+                label="Decoy Passenger Gene",
+            ),  # Decoy gene
+            plt.Line2D(
+                [0],
+                [0],
+                linestyle="-",
+                color="black",
+                linewidth=4,
+                label="Mutual Exclusivity",
+            ),
+        ],
+        labelspacing=2,
+        borderpad=1.5,
+        loc="center",
+        fontsize=24,  # Larger font size
+    )
+
+    plt.tight_layout(pad=0.5)
+    plt.savefig(f"figures/{subtype}_network_plots_across_methods.png", dpi=300)
