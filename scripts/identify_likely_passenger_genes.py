3cf7762ec950823e09b2afaf52c70fb86ec80983
ashuaibi7 <ashuaibi@princeton.edu>
2025-02-17 13:51:48 -0500

    feature: update likely passenger gene lists
    - Distinguish and generate gene level and event level
      (missense/nonsense) likely passenger gene lists per subtype

--- scripts/identify_likely_passenger_genes.py
+++ scripts/identify_likely_passenger_genes.py
@@ -11,33 +11,13 @@ import pandas as pd
 # ------------------------------------------------------------------------------------ #
 def build_argument_parser() -> ArgumentParser:
     """TODO: Add docstring."""
-    parser = ArgumentParser(description="Identify decoy genes")
-    parser.add_argument(
-        "-c",
-        "--cnt",
-        required=True,
-        help="Path to the count matrix file",
-    )
-    parser.add_argument(
-        "-d",
-        "--drvr",
-        required=True,
-        help="Path to the driver genes file",
-    )
-    parser.add_argument(
-        "-k",
-        "--top_k",
-        type=int,
-        default=100,
-        help="Rank cutoff for high mutation frequency",
-    )
-    parser.add_argument("-s", "--subtype", required=True, help="Name of cancer subtype")
-    parser.add_argument(
-        "-o",
-        "--out",
-        required=True,
-        help="Path to the output directory",
-    )
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--subtype", required=True)
+    parser.add_argument("-k", "--top_k", type=int, default=100)
+    parser.add_argument("-g", "--gene_level", action="store_true")
+    parser.add_argument("-o", "--out_dir", type=Path, required=True)
+    parser.add_argument("-c", "--cnt_mtx_fn", type=Path, required=True)
+    parser.add_argument("-d", "--putative_drivers_fn", type=Path, required=True)
     return parser
 
 
@@ -46,31 +26,34 @@ def build_argument_parser() -> ArgumentParser:
 # ------------------------------------------------------------------------------------ #
 def identify_likely_passenger_genes(
     cnt_df: pd.DataFrame,
-    driver_genes: list,
+    driver_genes: set,
     k: int,
-    fout: Path,
+    out_fn: Path,
 ) -> None:
     """TODO: Add docstring."""
     decoy_genes_df = cnt_df.drop(driver_genes, axis=1, errors="ignore")
     top_decoy_genes = (
         decoy_genes_df.sum(axis=0).sort_values(ascending=False).head(k).index
     )
-    with fout.open("w") as f:
+    with out_fn.open("w") as f:
         f.write("\n".join(top_decoy_genes))
 
 
 if __name__ == "__main__":
     parser = build_argument_parser()
     args = parser.parse_args()
-    cnt_df = pd.read_csv(args.cnt, index_col=0)
-    drvr_df = pd.read_csv(args.drvr, sep="\t", index_col=0)
-    driver_genes = set(drvr_df.index + "_M") | set(drvr_df.index + "_N")
-    dout = Path(args.out)
-    dout.mkdir(parents=True, exist_ok=True)
-    fout = dout / f"{args.subtype}.txt"
+    cnt_mtx_df = pd.read_csv(args.cnt_mtx_fn, index_col=0)
+    putative_drivers_df = pd.read_csv(args.putative_drivers_fn, sep="\t", index_col=0)
+    driver_genes = (
+        set(putative_drivers_df.index)
+        if args.gene_level
+        else set(putative_drivers_df.index + "_M")
+        | set(putative_drivers_df.index + "_N")
+    )
+    args.out_dir.mkdir(parents=True, exist_ok=True)
     identify_likely_passenger_genes(
-        cnt_df=cnt_df,
+        cnt_df=cnt_mtx_df,
         driver_genes=driver_genes,
         k=args.top_k,
-        fout=fout,
+        out_fn=args.out_dir / f"{args.subtype}.txt",
     )
